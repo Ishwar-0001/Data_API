398603ec2ca84e2f0b9ed2e427418f781619bb8e
abhisheksingh01-ai <abhishek.ai.learn@gmail.com>
2026-01-06 17:22:21 +0530

    render deploy ready

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
-// require("dotenv").config();
-// const app = require("./app");
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`🔥 Backend ${PORT} port par chal raha`);
-// });
-
-
 require("dotenv").config();
 const app = require("./app");
 
-module.exports = (req, res) => {
-  return app(req, res);
-};
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🔥 Backend ${PORT} port par chal raha`);
+});
+
+
+// require("dotenv").config();
+// const app = require("./app");
+
+// module.exports = (req, res) => {
+//   return app(req, res);
+// };
