2bfb270164e5867010e3c763efb5ce7d055c7a44
abhisheksingh01-ai <abhishek.ai.learn@gmail.com>
2026-01-07 11:11:28 +0530

    fix scrape route

--- src/app.js
+++ src/app.js
@@ -1,31 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const newsRoutes = require("./routes/scrapping.routes");
-
-// const app = express();
-
-// /* ---------------- Middleware ---------------- */
-// app.use(express.json());
-
-// // ✅ Allow Vite frontend
-// app.use(
-//   cors({
-//     origin: "https://frontend-weld-seven-a0rj6yqpp0.vercel.app",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
-
-// /* ---------------- Routes ---------------- */
-// app.use("/api/check", newsRoutes);
-
-// /* ---------------- Test Route ---------------- */
-// app.get("/", (req, res) => {
-//   res.send("Data api is running 🚀");
-// });
-
-// module.exports = app;
-
 const express = require("express");
 const scrapeRoutes = require("./routes/scrape.routes");
 const resultRoutes = require("./routes/result.routes");
@@ -33,7 +5,13 @@ const resultRoutes = require("./routes/result.routes");
 const app = express();
 
 app.use(express.json());
+
 app.use("/api/scrape", scrapeRoutes);
 app.use("/api/results", resultRoutes);
+
+// health check (VERY IMPORTANT FOR RENDER)
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,28 +1,10 @@
-// require("dotenv").config();
-// const app = require("./app");
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`🔥 Backend ${PORT} port par chal raha`);
-// });
-
-
-// // const app = require("../app");
-
-// // module.exports = app;
-
-
-require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./db/mongo");
 
 const PORT = process.env.PORT || 3000;
 
-(async () => {
-  await connectDB();
-
+connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
-})();
+});
